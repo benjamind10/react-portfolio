deb9d8d15ea454c868335ba998814bf1d1767853
Ben <benjamind10@pm.me>
2022-04-16 17:27:04 -0400

    render all project cards

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -1,22 +1,17 @@
-import Projects from '../projects/projects.component';
-
 const ProjectCard = ({ id, name, ...otherProps }) => {
+  const { liveUrl, image, techs } = otherProps;
   return (
-    <div>Terst{id}</div>
-    // <div className='project2'>
-    //   <a
-    //     href='https://benjamind10.github.io/local-coffee-finder/'
-    //     target='_blank'
-    //   >
-    //     <img src={require(`${image}`)} alt={name} />
-    //     <div className='project-text'>
-    //       {techs.map(tech => (
-    //         <p>{tech}</p>
-    //       ))}
-    //       <h3>{name}</h3>
-    //     </div>
-    //   </a>
-    // </div>
+    <div className='project'>
+      <a href={liveUrl} target='_blank' rel='noreferrer'>
+        <img src={require(`../../assets/${image}`)} alt={name} />
+        <div className='project-text'>
+          {techs.map((tech, i) => (
+            <p key={i}>{tech}</p>
+          ))}
+          <h3>{name}</h3>
+        </div>
+      </a>
+    </div>
   );
 };
 
--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -4,20 +4,63 @@ const projects = [
   {
     id: 1,
     name: 'COD Connect',
-    image: '../../assets/grp_project2.png',
+    image: 'grp_project2.png',
     techs: ['Node', 'Express', 'Handlebars'],
+    gitHub: 'https://benjamind10.github.io/gaming-blog',
+    liveUrl: 'https://rocky-sierra-14761.herokuapp.com/',
+  },
+  {
+    id: 2,
+    name: 'Local Coffee Finder',
+    image: 'grp_project1.png',
+    techs: ['HTML', 'CSS', 'JavaScript'],
     gitHub: 'https://benjamind10.github.io/local-coffee-finder/',
+    liveUrl: 'https://rocky-sierra-14761.herokuapp.com/',
+  },
+  {
+    id: 3,
+    name: '777otto',
+    image: 'img-2.png',
+    techs: ['React', 'Node', 'ES6'],
+    gitHub: 'https://benjamind10.github.io/777otto/',
+    liveUrl: 'https://determined-allen-00b7d2.netlify.app/',
+  },
+  {
+    id: 4,
+    name: 'Weather Station',
+    image: 'img-3.png',
+    techs: ['Vue', 'Rust', 'Node'],
+    gitHub: 'https://benjamind10.github.io/local-coffee-finder/',
+    liveUrl:
+      'https://weather.codihacks.org/stations/cbcb99ef-f20e-4484-93dd-ebda4a259481',
+  },
+  {
+    id: 5,
+    name: 'RGB Color Game',
+    image: 'img-5.png',
+    techs: ['HTML', 'CSS', 'JavaScript'],
+    gitHub: 'https://benjamind10.github.io/the-color-game',
+    liveUrl: 'https://rocky-sierra-14761.herokuapp.com/',
+  },
+  {
+    id: 6,
+    name: 'Monster Rolodex',
+    image: 'img-6.png',
+    techs: ['React', 'SASS', 'Node'],
+    gitHub: 'https://benjamind10.github.io/monsters-rolodex/',
     liveUrl: 'https://rocky-sierra-14761.herokuapp.com/',
   },
 ];
 
 const Projects = () => {
   return (
-    <div>
-      {projects.map(project => (
-        <ProjectCard {...project} />
-      ))}
-    </div>
+    <section className='work' id='work'>
+      <div className='column'>
+        {projects.map(project => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
+      </div>
+    </section>
   );
 };
 
