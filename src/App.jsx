2c65cf512306a21c0746e9a2785115d81510175c
Benjamin <benjamind10@pm.me>
2022-06-09 00:04:12 -0400

    reworking

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import './header.styles.css';
+
+const Header = () => {
+  return (
+    <section class='hero'>
+      <header class='hero-cta'>Web and App Developer</header>
+    </section>
+  );
+};
+
+export default Header;
