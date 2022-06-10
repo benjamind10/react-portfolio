78735e77eca10c3e63b2009f211996edbc0f9ade
Benjamin <benjamind10@pm.me>
2022-06-10 12:20:57 -0400

    added react spring

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,20 @@
+import { useSpring, animated } from 'react-spring';
+
 import './header.styles.css';
 
 const Header = () => {
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: 200,
+  });
+
   return (
     <div className='hero'>
       <div className='hero-cta'>
-        <span>Web and App Developer</span>
+        <animated.div style={props}>
+          Web and App Developer
+        </animated.div>
       </div>
     </div>
   );
